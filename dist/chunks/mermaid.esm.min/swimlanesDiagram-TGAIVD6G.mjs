5a2a20f040de0c1c72f34af3743bf97fee0a55b3
markdumay <61946753+markdumay@users.noreply.github.com>
2026-08-24 03:58:46 +0000

    fix: update Hugo module dependencies

---
+++ dist/chunks/mermaid.esm.min/swimlanesDiagram-TGAIVD6G.mjs
@@ -0,0 +1,8 @@
+import{a as r,b as o}from"./chunk-SPEABCGO.mjs";import"./chunk-AZZRMDJM.mjs";import"./chunk-RCXAM2GK.mjs";import"./chunk-VTHDQ7BV.mjs";import"./chunk-NUDPKBEF.mjs";import"./chunk-YBSNKKCV.mjs";import"./chunk-EICP7K5X.mjs";import"./chunk-LOO2ESPN.mjs";import"./chunk-UE256HPW.mjs";import"./chunk-MKUAOEP6.mjs";import"./chunk-QDFLYFYJ.mjs";import"./chunk-5IMINLNL.mjs";import"./chunk-ZNLK7HRR.mjs";import"./chunk-DZP67EKU.mjs";import"./chunk-2NMSUPJH.mjs";import"./chunk-VY2OSTJR.mjs";import"./chunk-STOV2HOB.mjs";import"./chunk-QJSWEUOL.mjs";import"./chunk-H7VHZCWX.mjs";import{a as e}from"./chunk-AQ6EADP3.mjs";var s=e(t=>`${r(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),l=s;var p=o({defaultLayout:"swimlane",styles:l});export{p as diagram};
